2d88d184f7ca8805749c0c62e8cf20041bdd5547
Eyal Etzyon <eyaletzyon@gmail.com>
2021-05-03 12:06:36 +0300

    Update CardGame.pyi

---
+++ BIG_pythonProject-master/Classes/CardGame.pyi
@@ -0,0 +1,13 @@
+from Player import *
+
+class CardGame:
+    def __init__(self, player_cards=10):
+
+        player1=Player(input("Enter player 1 name: ")), player_cards
+        player2=Player(input("Enter player 2 name: ")), player_cards
+        deck = DeckOfCards()
+
+
+
+    
+
