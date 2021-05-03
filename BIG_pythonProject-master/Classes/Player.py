4943c0dcd4fc907cb5729b984535e8cb8bb0780d
Eyal Etzyon <eyaletzyon@gmail.com>
2021-05-03 11:29:30 +0300

    dic pdate

--- BIG_pythonProject-master/Classes/Player.py
+++ BIG_pythonProject-master/Classes/Player.py
@@ -1,4 +1,22 @@
-
+from DeckOfCards import *
 
 class Player:
-    def __init__(self, name, ):
+    def __init__(self, name, cardsNum=10):
+        self.name = name
+        self.hand = []
+        if cardsNum>26:
+            cardsNum=26
+        self.cardsNum = cardsNum
+
+
+
+    def setHand(self, deck):
+        for i in range(self.cardsNum):
+            self.hand.append(deck.deal_one())
+
+P1 = Player("Eyal",10 )
+deck=DeckOfCards()
+P1.setHand(deck)
+print(P1.hand)
+
+
