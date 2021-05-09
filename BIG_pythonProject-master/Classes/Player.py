35a93d86315f50ff6f6c704ba0536ddc46881723
Eyal Etzyon <eyaletzyon@gmail.com>
2021-05-09 14:02:17 +0300

    init player test

--- BIG_pythonProject-master/Classes/Player.py
+++ BIG_pythonProject-master/Classes/Player.py
@@ -2,11 +2,18 @@ from DeckOfCards import *
 from Card import *
 
 class Player:
+    #create player with name and list of cards
     def __init__(self, name, cardsNum=10):
+        if type(name)!= str:
+            raise TypeError("not a string")
         self.name = name
         self.hand = []
+        if type(cardsNum)!= int:
+            raise TypeError("not an int")
         if cardsNum>26:
             cardsNum=26
+        if cardsNum<0:
+            raise TypeError("cards number cant be smaller than 0")
         self.cardsNum = cardsNum
 
     def __repr__(self):
--- BIG_pythonProject-master/Classes/test_Player.py
+++ BIG_pythonProject-master/Classes/test_Player.py
@@ -1,15 +1,66 @@
 from unittest import TestCase
-
-
+from Player import *
+from unittest import mock
 class TestPlayer(TestCase):
-    def test_set_hand(self):
-        self.fail()
 
-    def test_get_card(self):
-        self.fail()
+#test invalid name
+    def test_name_invalid(self):
+        try:
+            p = Player(123)
+            self.assertTrue(False)
+        except: TypeError("not a string")
+        self.assertTrue(True)
+#test valid name
+    def test_name_valid(self):
+            p = Player("eyal")
+            self.assertTrue(p.name=="eyal")
+            self.assertTrue(p.cardsNum==10)
 
-    def test_add_card(self):
-        self.fail()
+#test invalid input oc cards number
+    def test_CardsNum_invalid(self):
+        try:
+            p = Player("Eyal","string")
+            self.assertTrue(False)
+        except TypeError:
+                self.assertTrue(True)
 
-    def test_show(self):
-        self.fail()
+#test max cars num not in range
+    def test_CardsNum_invalid_num_min(self):
+        try:
+            p = Player("E", -1)
+            self.assertTrue(False)
+        except TypeError:
+            self.assertTrue(True)
+
+#test max cars num not in range
+    def test_CardsNum_invalid_num_max(self):
+            p = Player("Eyal", 27)
+            self.assertTrue(p.cardsNum==26)
+
+#test min cars num in range
+    def test_CardsNum_valid_num_min(self):
+            p = Player("E", 0)
+            self.assertTrue(p.cardsNum==0)
+
+
+        # test max cars num in range
+    def test_CardsNum_valid_num_max(self):
+        p = Player("Eyal", 26)
+        self.assertTrue(p.cardsNum == 26)
+
+
+
+
+
+
+    # def test_set_hand(self):
+    #     self.fail()
+    #
+    # def test_get_card(self):
+    #     self.fail()
+    #
+    # def test_add_card(self):
+    #     self.fail()
+    #
+    # def test_show(self):
+    #     self.fail()
